fc713ab102d99a353b1af4c7cbf1585d0e6bad76
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-07-29 14:18:01 -0300

    Integrate new Transitions System to the game

--- main_menu/SceneChanger.gd
+++ main_menu/SceneChanger.gd
@@ -9,6 +9,7 @@ class_name SceneChanger
 # public variables - order: export > normal var > onready 
 # private variables - order: export > normal var > onready
 export(String, FILE, "*.tscn") var _next_scene_path: String = ""
+export(Resource) var transition_data = null
 
 ### ---------------------------------------
 
@@ -29,7 +30,13 @@ func _ready():
 
 ### Private Methods -----------------------
 func _on_owner_pressed() -> void:
+	if transition_data != null and transition_data is EHJ_TransitionData:
+		EHJ_Transitions.transition_data = transition_data
+	
+	EHJ_Transitions.play_transition_in()
+	yield(EHJ_Transitions, "transition_mid_point_reached")
 	get_tree().change_scene(_next_scene_path)
+	EHJ_Transitions.play_transition_out()
 
 ### ---------------------------------------
 
