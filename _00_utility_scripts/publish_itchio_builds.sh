8f95c091fb77521cc546644dfd84dab0b3204283
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-08-15 21:28:09 -0300

    Update publish_itchio_builds.sh to make customizing it for other projects easier

--- _00_utility_scripts/publish_itchio_builds.sh
+++ _00_utility_scripts/publish_itchio_builds.sh
@@ -2,6 +2,9 @@
 cd "$(dirname "$0")"
 cd ..
 
+itch_game_adress=eh-jogos/shaderexperiments
+project_name="ShaderExperiments"
+
 project_settings="project.godot"
 export_configs="export_presets.cfg"
 build_folder=$(cat $project_settings | grep "^config/build_folder" | cut -d'=' -f2)
@@ -10,49 +13,51 @@ game_version=$(cat $project_settings | grep "^config/version" | cut -d'=' -f2)
 game_version=$(sed -e 's/^"//' -e 's/"$//' <<< $game_version)
 project_path=$(pwd)
 base_builds_path="$(dirname $project_path)/GameBuilds/$build_folder/Release"
-itch_game_adress=eh-jogos/shaderexperiments
 
 builds_to_push=$1
 
-if [[ -z $builds_to_push || $builds_to_push = "all" ]]
-then
-	builds_to_push="all"
-elif [[ $builds_to_push = "linux" ]]
-then
-	builds_to_push="linux"
-elif [[ $builds_to_push = "win" || $builds_to_push = "windows" ]]
-then
-	builds_to_push="win"
-elif [[ $builds_to_push = "mac" || $builds_to_push = "osx" ]]
-then
-	builds_to_push="osx"
-elif [[ $builds_to_push = "html" || $builds_to_push = "web" ]]
-then
-	builds_to_push="html"
-else
-	echo "Unrecognized option for builds_to_push: $builds_to_push | changing to 'all'"
-	builds_to_push=false
-fi
+case $builds_to_push in
+	"all")
+		builds_to_push="all"
+		;;
+	"linux" | "lin")
+		builds_to_push="linux"
+		;;
+	"win" | "windows")
+		builds_to_push="win"
+		;;
+	"mac" | "osx")
+		builds_to_push="osx"
+		;;
+	"html" | "web")
+		builds_to_push="html"
+		;;
+	*)
+		echo "Unrecognized option for builds_to_push: $builds_to_push | changing to default ('all')"
+		builds_to_push="all"
+		;;
+esac
+
 
 # In the future, maybe try to use the same folder structure you use for steam, but add some ignores to separate the versions
 # for OSX you can just send the zip directly
 function push_linux {
 	echo $itch_game_adress
-	butler push --userversion=$game_version $base_builds_path/ShaderExperimentsLinux32 $itch_game_adress\:linux32
-	butler push --userversion=$game_version $base_builds_path/ShaderExperimentsLinux64 $itch_game_adress\:linux64
+	butler push --userversion=$game_version $base_builds_path/$project_name+Linux32 $itch_game_adress\:linux32
+	butler push --userversion=$game_version $base_builds_path/$project_name+Linux64 $itch_game_adress\:linux64
 }
 
 function push_windows {
-	butler push --userversion=$game_version $base_builds_path/ShaderExperimentsWindows32 $itch_game_adress\:windows32
-	butler push --userversion=$game_version $base_builds_path/ShaderExperimentsWindows64 $itch_game_adress\:windows64
+	butler push --userversion=$game_version $base_builds_path/$project_name+Windows32 $itch_game_adress\:windows32
+	butler push --userversion=$game_version $base_builds_path/$project_name+Windows64 $itch_game_adress\:windows64
 }
 
 function push_osx {
-	butler push --userversion=$game_version $base_builds_path/ShaderExperimentsOSX $itch_game_adress\:osx-universal
+	butler push --userversion=$game_version $base_builds_path/$project_name+OSX $itch_game_adress\:osx-universal
 }
 
 function push_html {
-	butler push --userversion=$game_version $base_builds_path/ShaderExperimentsHTML5 $itch_game_adress\:html
+	butler push --userversion=$game_version $base_builds_path/$project_name+HTML5 $itch_game_adress\:html
 }
 
 
